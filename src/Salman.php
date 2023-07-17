f98db7be15794bf928004b5f001a029a18c00da8
rafaWPP <newgestorweb@gmail.com>
2023-07-17 19:58:55 -0300

    atu

--- src/Salman.php
+++ src/Salman.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace WebTech\WhatsAppApi\Salman;
+namespace WebTech\WhatsAppApi;
 
 use WebTech\WhatsAppApi\Config\Config;
 use WebTech\WhatsAppApi\Util\HttpClient;
@@ -58,5 +58,73 @@ class Salman
     public function fetchAll()
     {
         return $this->httpClient->get('/instance/list');
+    }
+
+    public function sendText($number, $message)
+    {
+        $data = [
+            'id' => $number,
+            'message' => $message
+        ];
+
+        return $this->httpClient->post('/message/text?key=' . $this->key, $data);
+    }
+
+    public function sendMedia($number, $caption, $media)
+    {
+        $mediaMessage = $this->getMediaMessage($caption, $media);
+        $data = [
+            'id' => $number,
+            'url' => $mediaMessage['media'],
+            'type' => $mediaMessage['mediatype'],
+            'mimetype' => $mediaMessage['mimetype'],
+            'caption' => $mediaMessage['caption']
+        ];
+
+        return $this->httpClient->post('/message/mediaurl?key=' . $this->key, $data);
+    }
+
+    public function sendButtons($number, $btndata)
+    {
+        return $this->httpClient->post('/message/button?key=' . $this->key, $btndata);
+    }
+
+    public function sendMediaButtons($number, $btndata)
+    {
+        return $this->httpClient->post('/message/MediaButton?key=' . $this->key, $btndata);
+    }
+
+    public function sendList($number, $msgdata)
+    {
+        return $this->httpClient->post('/message/list?key=' . $this->key, $msgdata);
+    }
+
+    private function getMediaMessage($caption, $media)
+    {
+        $extension = pathinfo($media, PATHINFO_EXTENSION);
+        $imageExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+        $documentExtensions = ['pdf', 'doc', 'docx'];
+        $videoExtensions = ['mp4', 'avi', 'mov'];
+        $audioExtensions = ['mp3', 'wav'];
+        $mediatype = '';
+
+        if (in_array($extension, $imageExtensions)) {
+            $mediatype = 'image';
+        } elseif (in_array($extension, $documentExtensions)) {
+            $mediatype = 'document';
+        } elseif (in_array($extension, $videoExtensions)) {
+            $mediatype = 'video';
+        } elseif (in_array($extension, $audioExtensions)) {
+            $mediatype = 'audio';
+        } else {
+            throw new Exception("Tipo de arquivo inválido");
+        }
+
+        return [
+            'mediatype' => $mediatype,
+            'fileName' => $media,
+            'caption' => $caption,
+            'media' => $media
+        ];
     }
 }
